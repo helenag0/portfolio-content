02f5e9b7d593c6d371ca43cbad768619fa7566b6
Helena Grobel <73837922+helenag0@users.noreply.github.com>
2022-04-13 21:34:38 -0400

    Add files via upload

--- web.js
+++ web.js
@@ -6,30 +6,6 @@ var originalText = document.querySelector(".jstext");
 // console.log(splitText)
 
 
-// function mouseOverHeader() {
-//     console.log("mouse over")
-//     text.textContent = "";
-//     for(let i=0; i < splitText.length; i++) {
-//         text.innerHTML += "<span class='jsspan'>" + splitText[i] + "</span";
-//     }
-//     let char = 0;
-//     let timer = setInterval(onTick, 50);
-
-//     function onTick() {
-//         const span = text.querySelectorAll('span')[char];
-//         span.classList.add('fade');
-//         char++
-//         if(char == splitText.length) {
-//             complete();
-//             return;
-//         }
-//     }
-
-//     function complete() {
-//         clearInterval(timer);
-//         timer = null;
-//     }
-// }
 
 console.log("mouse over")
 text.textContent = "";
